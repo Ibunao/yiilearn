a3ffe77e1ab2414d3404463de0ca0ad5e1575085
unknown <dingran@uki.com>
2017-09-20 18:29:59 +0800

    2017-09-20

--- basic/controllers/TestController.php
+++ basic/controllers/TestController.php
@@ -9,12 +9,16 @@ class TestController extends Controller
 {
 	public function actionTest()
 	{
+		$ding = [1,2,3];
 		//获取类名
-		// echo $this->className();//app\controllers\TestController
-
+		echo $this->className();//app\controllers\TestController
+		foreach ($ding as $key => $value) {
+			$key;
+		}
 		//别名alisas
-		Yii::setAlias('@ding/ran/bunao', 'basic/ding/ran/bunao');
-		var_dump($this->aliases());
+		// Yii::setAlias('@ding/ran/bunao', 'basic/ding/ran/bunao ');
+
+		// var_dump($this->aliases());
 		echo Yii::t('app', 'ding');
 	}
 
